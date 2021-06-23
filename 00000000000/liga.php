affc0a655385536a9a7fde414e6ab602dbfc74ab
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2021-06-23 11:23:09 +0200

    część HTML

---
+++ 00000000000/liga.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>piłka nożna</title>
+    <link rel="stylesheet" href="styl2.css">
+</head>
+
+<body>
+    <header>
+        <h3>Reprezentacja Polski w Piłce Nożnej</h3>
+        <img src="obraz1.jpg" alt="reprezentacja">
+    </header>
+    <div>
+        <div id="lewy">
+            <form action="liga.php" method="post">
+                <select>
+                    <option value="">Bramkarze</option>
+                    <option value="">Obrońcy</option>
+                    <option value="">Pomocnicy</option>
+                    <option value="">Napastnicy</option>
+                </select>
+                <button type="submit">Zobacz</button>
+                <img src="zad2.png" alt="piłka">
+                <p>Autor: 00000000000</p>
+            </form>
+        </div>
+        <div id="prawy">
+            <ol>
+                <li></li>
+            </ol>
+        </div>
+    </div>
+    <main>
+        <h3>Liga mistrzów</h3>
+    </main>
+    <div id="liga">
+    </div>
+</body>
+
+</html>
